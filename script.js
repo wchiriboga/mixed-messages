14225fc1c94c5ceda31cdcb389b9f8daa1ba3080
William Chiriboga <williamchiriboga@Williams-MacBook-Pro.local>
2021-05-24 19:37:30 -0400

    Add message compnents as objects

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-message = `Hello World`;
-console.log(message);
+// storing science jokes in object arrays, object is named after joke subject.
+// factory function for jokes
+const jokeObjects = (joke) => {
+    return {
+        joke
+    }
+}
+
+Helium = jokeObjects(`I was reading a book on helium. I couldnt't put it down!`);
+Earthquake = jokeObjects(`One tectonic plate bumped into another and said Sorry My Fault!`);
+Light = jokeObjects(`Since light travels faster than sound, people may appear bright until they speak.`);
+jokes = [Helium, Earthquake, Light];
+console.log(Helium);
+console.log(Earthquake);
+console.log(Light);
+console.log(jokes);
